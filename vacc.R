95896e4f6ca2421e83665a0c978482b6992c3c96
DiNOV-Tokyo <dddd3ddd@gmail.com>
2021-05-28 18:58:11 +0900

    data rev

--- vacc.R
+++ vacc.R
@@ -34,20 +34,40 @@ plot(jpn.shp)
 vacc_dat <- read.csv("vacc.csv", fileEncoding = "UTF-8") %>% 
   slice(2:48)
 
+vacc_dat %>% left_join(jpn)
+jpn.shp@data[["NL_NAME_1"]]
 # 2-1.都道府県名などで一致する列を見つける（toupper()は英文字列を大文字にする関数）
 #table(toupper(jpn.shp@data[["NAME_1"]]) == toupper(df_japan_census$region))
 # 長崎が'Naoasakiになっているスペルミスなどで一致しない行が２つあったが，データの順番は問題ないのでプロットに使用可能
 
 
 # 塗る色（連続値のカラーパレット）をセット
-pal <- colorNumeric("YlOrRd", domain=vacc_dat$vacc, reverse=F)
+pal <- colorNumeric("YlOrRd", domain=jpn.shp@data$vacc, reverse=F)
 
 # マウスオーバー時の表示内容を設定（sprintf()で実数表記など指定）
 labels <- sprintf("<strong>%s</strong><br/>接種回数：%5.0f<br/>内1回目：%5.0f<br/>内2回目：%5.0f",
                   paste0(jpn.shp@data$NL_NAME_1),
-                  vacc_dat$vacc,
-                  vacc_dat$vacc1,
-                  vacc_dat$vacc2) %>% lapply(htmltools::HTML)
+                  jpn.shp@data$vacc,
+                  jpn.shp@data$vacc1,
+                  jpn.shp@data$vacc2) %>% lapply(htmltools::HTML)
+
+library("sf")
+va <- vacc_dat %>% mutate(NL_NAME_1=pref)
+sp_data <- jpn.shp@data %>% 
+  inner_join(va, by="NL_NAME_1") %>% 
+  select(pref, vacc, vacc1, vacc2)
+
+  
+jpn.shp@data$pref <- sp_data$pref
+jpn.shp@data$vacc <- sp_data$vacc
+jpn.shp@data$vacc1 <- sp_data$vacc1
+jpn.shp@data$vacc2 <- sp_data$vacc2
+
+jpn.shp@data$pref
+
+d_tokyo_ <- d_tokyo %>%
+  mutate(市区名 = `Encoding<-`(市区名, "UTF-8")) %>%
+  inner_join(d_chika) %>% st_as_sf(.)
 
 # 地図にプロット
 jpn.shp %>% 
@@ -59,7 +79,7 @@ jpn.shp %>%
   # addPolygons() : 塗り分け地図の描画
   addPolygons(fillOpacity = 0.5,
               weight=1,
-              fillColor = ~pal(vacc_dat$vacc),
+              fillColor = ~pal(jpn.shp@data$vacc),
               color = "orange",
               label = labels,
               labelOptions = labelOptions(
@@ -72,5 +92,5 @@ jpn.shp %>%
                 fillOpacity = 0.5,
                 bringToFront = TRUE)
               ) %>% 
-  addLegend("bottomright", pal = pal, values = ~vacc_dat$vacc,
+  addLegend("bottomright", pal = pal, values = ~jpn.shp@data$vacc,
             title = "新型コロナウイルス　ワクチン接種状況"  )
